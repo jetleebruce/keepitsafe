ffadb87bad4625698ce78cf46c37d13034434fab
Serge Modin <modinsv@yandex.ru>
2020-02-08 15:00:42 +0300

    Validation setting
    Added Express Validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 //@route     POST  api/users
 //@desc      Register the user
 //@access    Public
-router.post("/", (req, res) => {
-  res.send("Register the user");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required")
+      .not()
+      .isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check("password", "Please enter more chracters").isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed')
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DataBase
 connectDB();
 
+//Init middleware
+app.use(express.json({extended: false}))
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcom to the contact keeper api" })
 );
