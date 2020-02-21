0f9ddbd68a2e481a40cf8c5544ceb60ea67e3abc
Serge Modin <modinsv@yandex.ru>
2020-02-21 17:57:41 +0300

    Contact Filter Form & State

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,18 +1,25 @@
 import React, { Fragment, useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
-import ContactItem from './ContactItem'
+import ContactItem from "./ContactItem";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
+  const { contacts, filtered } = contactContext;
+  if (contacts.length === 0) {
+    return <h4>Please add contact</h4>;
+  }
 
-  const {contacts} = contactContext
   return (
-      <Fragment>
-          {contacts.map(contact => (
-              <ContactItem key={contact.id}contact={contact}/>
+    <Fragment>
+      {filtered !== null
+        ? filtered.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
           ))}
-      </Fragment>
-  )
+    </Fragment>
+  );
 };
 
 export default Contacts;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Contacts from '../contacts/Contacts'
 import ContactForm from '../contacts/ContactForm'
+import ContactFilter from "../../components/contacts/СontactFilter";
 
  const Home = () => {
     return (
@@ -9,6 +10,7 @@ import ContactForm from '../contacts/ContactForm'
                 <ContactForm/>
             </div>
             <div>
+                <ContactFilter/>
                 <Contacts/>
             </div>
         </div>
