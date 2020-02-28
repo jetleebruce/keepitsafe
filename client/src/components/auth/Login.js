c44f71ab5c8e0ff7d7d79279cff34178097509b3
Serge Modin <modinsv@yandex.ru>
2020-02-28 16:24:28 +0300

    User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Login = () => {
+const Login = (props) => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    //eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
   const [user, setUser] = useState({
     email: "",
-    password: "",
+    password: ""
   });
 
-  const {email, password } = user;
+  const { email, password } = user;
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Login submit");
+    if (email === "" || password === "") {
+      setAlert("Please, fill in all fields", "danger");
+    } else {
+login({
+  email,
+  password
+})
+    }
   };
   return (
     <div className='form-container'>
@@ -20,7 +45,6 @@ const Login = () => {
         Account <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
-        
         <div className='form-group'>
           <label htmlFor='email'>Email Address</label>
           <input
@@ -28,6 +52,7 @@ const Login = () => {
             name='email'
             value={email}
             onChange={onChange}
+            required
           ></input>
         </div>
         <div className='form-group'>
@@ -37,9 +62,10 @@ const Login = () => {
             name='password'
             value={password}
             onChange={onChange}
+            required
           ></input>
         </div>
-       
+
         <input
           type='submit'
           value='Register'
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -62,7 +62,27 @@ const AuthState = props => {
     }
   };
   //Login user
-  const login = () => console.log("login");
+   const login = async formData => {
+     const config = {
+       headers: {
+         "Content-type": "application/json"
+       }
+     };
+     try {
+       const res = await axios.post("api/auth", formData, config);
+
+       dispatch({
+         type: LOGIN_SUCCESS,
+         payload: res.data
+       });
+       loadUser();
+     } catch (err) {
+       dispatch({
+         type: LOGIN_FAIL,
+         payload: err.response.data.msg
+       });
+     }
+   };
   //Logout user
   const logout = () => console.log("logout");
   //Clear errors
