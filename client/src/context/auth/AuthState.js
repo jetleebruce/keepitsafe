55c5b19c6a58c891a161d01e7ce1393e5db5ccf0
Serge Modin <modinsv@yandex.ru>
2020-03-01 13:09:35 +0300

    Logout & Navbar

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -84,7 +84,7 @@ const AuthState = props => {
      }
    };
   //Logout user
-  const logout = () => console.log("logout");
+  const logout = () => dispatch({type: LOGOUT});
   //Clear errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
